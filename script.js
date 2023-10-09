827645b6e2c4c07a92df8345185afa84e8f1ee16
Sritam Behera <bsritam227@gmail.com>
2023-10-09 08:54:34 +0530

    Netflix Clone project added

--- script.js
+++ script.js
@@ -17,11 +17,16 @@ window.onscroll=()=>{
         let offset =sec.offsetTop - 150;
         let height=sec.offsetHeight;
         let id=sec.getAttribute('id');
+        // console.log(id);
 
         if(top>=offset && top<offset+height){
-            navLinks.forEach(links=>{
-                links.classList.remove('active');
-                document.querySelector('.page-links[href*=' + id +']').classList.add('active');
+            navLinks.forEach((links)=>{
+                // links.classList.remove('active-link');
+                links.style.color="white";
+        
+                // document.querySelector('.page-links[href*=' + id +']').classList.add('active-link');
+                document.querySelector('.page-links[href*=' + id +']').style.color="aqua"
+                console.log(document.querySelector('.page-links[href*=' + id +']'));
             })
         }
     });
