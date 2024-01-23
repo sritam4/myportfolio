1f0b7314ce363b82002ce0f035b7b41e1a1517e3
Sritam Behera <bsritam227@gmail.com>
2024-01-23 12:40:50 +0530

    updated resume and photo, added skill section

--- script.js
+++ script.js
@@ -1,51 +1,62 @@
 // toggle icon navbar
 
-let menuIcon= document.getElementById("menu-icon");
-let navbarRight= document.querySelector(".right");
-menuIcon.onclick=()=>{
-    menuIcon.classList.toggle('bx-x');
-    navbarRight.classList.toggle('active');
-}
-
-// scroll selection active link
-let section=document.querySelectorAll('section');
-let navLinks=document.querySelectorAll('.page-links');
-
-window.onscroll=()=>{
-    section.forEach(sec=>{
-        let top=window.scrollY;
-        let offset =sec.offsetTop - 150;
-        let height=sec.offsetHeight;
-        let id=sec.getAttribute('id');
-
-        if(top>=offset && top<offset+height){
-            navLinks.forEach((links)=>{
-                // links.classList.remove('active-link');
-                links.style.color="white";
-                // document.querySelector('.page-links[href*=' + id +']').classList.add('active-link');
-                document.querySelector('.page-links[href*=' + id +']').style.color="aqua";
-            })
-        }
-    });
-    // remove menuicon and navbar when click navbar link///
-    menuIcon.classList.remove('bx-x');
-    navbarRight.classList.remove('active');
+let menuIcon = document.getElementById("menu-icon");
+let navbarRight = document.querySelector(".right");
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navbarRight.classList.toggle("active");
 };
 
+// scroll selection active link
+let section = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll(".page-links");
+
+window.onscroll = () => {
+  section.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        // links.classList.remove('active-link');
+        links.style.color = "white";
+        // document.querySelector('.page-links[href*=' + id +']').classList.add('active-link');
+        document.querySelector(".page-links[href*=" + id + "]").style.color =
+          "aqua";
+      });
+    }
+  });
+  // remove menuicon and navbar when click navbar link///
+  menuIcon.classList.remove("bx-x");
+  navbarRight.classList.remove("active");
+};
 
 ///show more about me on click read more button
-let moreStuff=document.getElementById("more");
-let readMore=document.getElementById("readMore");
-let readLess=document.getElementById("readLess");
+let moreStuff = document.getElementById("more");
+let readMore = document.getElementById("readMore");
+let readLess = document.getElementById("readLess");
 
-readMore.addEventListener("click",()=>{
-    moreStuff.classList.toggle("hide");
-    readMore.classList.add("hide");
-    readLess.classList.toggle("hide")
-})
+readMore.addEventListener("click", () => {
+  moreStuff.classList.toggle("hide");
+  readMore.classList.add("hide");
+  readLess.classList.toggle("hide");
+});
 
-readLess.addEventListener("click",()=>{
-    moreStuff.classList.toggle("hide");
-    readMore.classList.remove("hide");
-    readLess.classList.toggle("hide")
-})
+readLess.addEventListener("click", () => {
+  moreStuff.classList.toggle("hide");
+  readMore.classList.remove("hide");
+  readLess.classList.toggle("hide");
+});
+
+// slider logics
+
+// const slides = document.querySelectorAll(".slider-cards");
+
+// console.log(slides);
+
+// slides.forEach((item, index) => {
+//   item.style.left = `${index * 20}%`;
+//   console.log(item + "  " + index * 100);
+// });
